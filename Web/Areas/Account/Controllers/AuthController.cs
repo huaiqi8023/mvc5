5a67ce333e8baae9f80b57157dcc913f13c5b597
全美中餐 <huaiqi8023@hotmail.com>
2015-12-11 18:16:50 +0800

    0

---
+++ Web/Areas/Account/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Areas.Account.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Account/Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
