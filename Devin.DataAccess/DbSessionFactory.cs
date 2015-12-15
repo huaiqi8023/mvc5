e1c0578915f0a7b1767bcbe2dd1f216eaf1c3d6d
全美中餐 <huaiqi8023@hotmail.com>
2015-12-15 15:19:45 +0800

    0

---
+++ Devin.DataAccess/DbSessionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Devin.IDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devin.DataAccess
+{
+    public static class DbSessionFactory
+    {
+        public static IDbSession GetCurrentSession()
+        {
+            IDbSession _dbSession = CallContext.GetData("DbSession") as DbSession;
+            if (_dbSession == null)
+            {
+                _dbSession = new DbSession();
+                CallContext.SetData("DbSession", _dbSession);
+            }
+            return _dbSession;
+        }
+    }
+}
