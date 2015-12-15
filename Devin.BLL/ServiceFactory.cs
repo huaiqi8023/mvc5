d3090b1cd64ee1e0301b8e7d04c475e12124c5c1
全美中餐 <huaiqi8023@hotmail.com>
2015-12-15 15:20:04 +0800

    0

--- Devin.BLL/ServiceFactory.cs
+++ Devin.BLL/ServiceFactory.cs
@@ -9,47 +9,47 @@ namespace Devin.BLL
 {
     public static class ServiceFactory
     {
-	        public static Idt_managerService dt_managerService
+        public static Idt_managerService dt_managerService
         {
             get
             {
                 return new dt_managerService();
             }
         }
-		        public static Idt_manager_logService dt_manager_logService
+        public static Idt_manager_logService dt_manager_logService
         {
             get
             {
                 return new dt_manager_logService();
             }
         }
-		        public static Idt_manager_roleService dt_manager_roleService
+        public static Idt_manager_roleService dt_manager_roleService
         {
             get
             {
                 return new dt_manager_roleService();
             }
         }
-		        public static Idt_manager_role_valueService dt_manager_role_valueService
+        public static Idt_manager_role_valueService dt_manager_role_valueService
         {
             get
             {
                 return new dt_manager_role_valueService();
             }
         }
-		        public static Idt_navigationService dt_navigationService
+        public static Idt_navigationService dt_navigationService
         {
             get
             {
                 return new dt_navigationService();
             }
         }
-		        public static Idt_sms_templateService dt_sms_templateService
+        public static Idt_sms_templateService dt_sms_templateService
         {
             get
             {
                 return new dt_sms_templateService();
             }
         }
-		    }
+    }
 }
