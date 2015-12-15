49b1bde05a828bddc8678bc4a76f9d14dfd63c78
全美中餐 <huaiqi8023@hotmail.com>
2015-12-15 15:19:08 +0800

    0

---
+++ Devin.IBLL/Idt_managerService.cs
@@ -0,0 +1,15 @@
+﻿using Devin.IBLL.IBaseService;
+using Devin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devin.IBLL
+{
+    public partial interface Idt_managerService : IBaseService<dt_manager>
+    {
+        dt_manager checkUserLogin(dt_manager userinfo);
+    }
+}
